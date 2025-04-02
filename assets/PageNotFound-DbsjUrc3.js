3a23fc727e65402ada2b5580c6266e6c569a1d46
apptechdesk <apptechdesk@users.noreply.github.com>
2025-04-02 16:36:59 +0000

    deploy: 942abc75d8e0967a9e1851c8d68404c2f57ed63c

---
+++ assets/PageNotFound-DbsjUrc3.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Blu9k9kt.js";function n(){return r.jsx("div",{className:"overall-bg align-center",children:r.jsxs("div",{className:"error-txt",children:[r.jsx("p",{children:"404 Error"}),r.jsx("p",{children:"page not found"})]})})}export{n as default};
