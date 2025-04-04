bd066807c250cd4978bc0211a691416a1f3556cf
Sivamani-18 <sivasubramaniyam09@gmail.com>
2025-04-04 07:12:04 +0000

    Synced Build from ePlatform master: Fri Apr  4 07:12:04 UTC 2025

---
+++ site/assets/PageNotFound-2Up1XWbI.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-t5R8MH8e.js";function n(){return r.jsx("div",{className:"overall-bg align-center",children:r.jsxs("div",{className:"error-txt",children:[r.jsx("p",{children:"404 Error"}),r.jsx("p",{children:"page not found"})]})})}export{n as default};
