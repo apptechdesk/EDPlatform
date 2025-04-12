4a367c418e1471df2f8c1ef3c97a08f5dddaa1b9
apptechdesk <apptechdesk@users.noreply.github.com>
2025-04-12 07:15:36 +0000

    deploy: e4a4ff606d56bd597eae4a1185917ebc5348080b

---
+++ assets/PageNotFound-TRQW3JQK.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-djq0z_Y8.js";function n(){return r.jsx("div",{className:"overall-bg align-center",children:r.jsxs("div",{className:"error-txt",children:[r.jsx("p",{children:"404 Error"}),r.jsx("p",{children:"page not found"})]})})}export{n as default};
